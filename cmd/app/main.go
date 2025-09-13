0500fa6136ed72cbc0388e4ad511d52a0ac1e6f9
TierThree <rameezshaukat@gmail.com>
2025-09-13 10:04:56 -0500

    test API

---
+++ cmd/app/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type test struct {
+	ID string
+}
+
+var ids = []test{
+	{ID: "1"},
+}
+var tracker = 2
+
+func testValues(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, ids)
+}
+
+func setValues(c *gin.Context) {
+	ids = append(ids, test{ID: string(tracker)})
+	tracker = tracker + 1
+}
+
+func main() {
+	router := gin.Default()
+	fmt.Sprintf("Type is: %T\n", router)
+	router.GET("/test", testValues)
+	router.POST("/addtest", setValues)
+
+	router.Run("localhost:8080")
+}
